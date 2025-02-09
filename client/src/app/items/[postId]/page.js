81dbe62f533dd508270904a2f19f23dc3288ad68
Fayaz Shaikh <61674751+fayaz12g@users.noreply.github.com>
2025-02-09 01:33:06 -0500

    tag fixes

--- client/src/app/items/[postId]/page.js
+++ client/src/app/items/[postId]/page.js
@@ -3,7 +3,6 @@
 import { useEffect, useState } from "react";
 import { useRouter, useParams } from "next/navigation"; // For Next.js 13 App Router; if using the Pages Router, use useRouter from 'next/router'
 import Header from "../../components/header";
-import Link from "next/link";
 import Background from "../../components/background";
 import { volunteerService } from "../../services/volunteerService";
 
@@ -67,21 +66,16 @@ export default function FullPostPage() {
             ))}
           </div>
         )}
-         <div className="flex space-x-4">
-              {post.link && (
-                  <a
-                    href={post.link}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="px-4 py-2 bg-blue-600 hover:bg-blue-700 rounded-md"
-                  >
-                    Visit Link
-                  </a>
-                )}
-                <Link href={`/items/${post.id}`} className="px-4 py-2 bg-green-600 hover:bg-green-700 rounded-md">
-                    View Full Post
-                </Link>
-                </div>
+        {post.link && (
+          <a
+            href={post.link}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="px-4 py-2 bg-blue-600 hover:bg-blue-700 rounded-md"
+          >
+            Visit Link
+          </a>
+        )}
       </div>
     </div>
   );
