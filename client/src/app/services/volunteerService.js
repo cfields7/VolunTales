0d70ef429b4fa6746769f44b9aca75687ae24e33
Fayaz Shaikh <61674751+fayaz12g@users.noreply.github.com>
2025-02-08 23:20:34 -0500

    Create volunteerService.js

---
+++ client/src/app/services/volunteerService.js
@@ -0,0 +1,38 @@
+const serverPath = "https://cfhc.fly.dev/api";
+
+export const requestsService = {
+
+  async getTime() {
+    const res = await fetch(serverPath + '/get/time', {
+      method: "GET",
+      headers: {
+        "Content-type": "application/json; charset=UTF-8"
+      }
+    });
+
+    return Promise.resolve(res);
+  },
+
+  async getFinance() {
+    const res = await fetch(serverPath + '/get/finance', {
+      method: "GET",
+      headers: {
+        "Content-type": "application/json; charset=UTF-8"
+      }
+    });
+
+    return Promise.resolve(res);
+  },
+
+  async getItems() {
+    const res = await fetch(serverPath + '/get/item', {
+      method: "GET",
+      headers: {
+        "Content-type": "application/json; charset=UTF-8"
+      }
+    });
+
+    return Promise.resolve(res);
+  }
+
+}
