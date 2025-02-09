907907c781020b51d3cd7dcae3a2b37bd6dc110e
Fayaz Shaikh <61674751+fayaz12g@users.noreply.github.com>
2025-02-09 05:58:08 -0500

    Update page.js

--- client/src/app/home/page.js
+++ client/src/app/home/page.js
@@ -16,7 +16,8 @@ const HomePage = () => {
       <br />
       <br />
       <div className="max-w-6xl mx-auto p-6">
-        <h1 className="text-4xl font-bold text-white text-center mb-12">Local Resources</h1>
+        <h1 className="text-4xl font-bold text-white text-center">Local Resources</h1>
+        <h1 className="text-xl font-itallic text-white text-center mb-12"><i>(Raleigh, NC)<i/></i></h1>
         
         {/* Action Buttons */}
         <div className="flex justify-center gap-4 mb-12">
